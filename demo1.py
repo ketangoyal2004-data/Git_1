a10059b4db785bc8686b3b5ce213cbb4de60d81d
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-01-29 08:37:26 +0530

    This is a added new files

--- demo1.py
+++ demo1.py
@@ -1,1 +1,8 @@
 print("Hello jee-1")
+
+class std:
+    def name(self):
+        print("Harry")
+
+    def course(self):
+        print("B.Tech")
--- demo2.py
+++ demo2.py
@@ -1,1 +1,10 @@
 print("Hello Jee-2")
+
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x - y
+
+add(10,2)
+sub(4,4)
